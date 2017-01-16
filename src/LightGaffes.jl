d4b979a28c33067707d6a9fc4a1084678830c012
Chelsea Sidrane <csidrane@stanford.edu>
2017-01-16 14:47:22 -0800

    Added first scenario generating function

--- src/LightGaffes.jl
+++ src/LightGaffes.jl
@@ -1,5 +1,14 @@
 module LightGaffes
 
-# package code goes here
+function generate_scenario()
+    subject = ["grandma", "I", "mom"]
+    verb = ["tripped", "insulted", "spilled coffee on", "picked my nose in front of"]
+    object = ["my teacher", "the cashier", "my kid", "the president"]
+
+    print(subject[Int(ceil(rand()*length(subject)))], " ", 
+        verb[Int(ceil(rand()*length(verb)))], " ",
+        object[Int(ceil(rand()*length(subject)))])
+
+end
 
 end # module
